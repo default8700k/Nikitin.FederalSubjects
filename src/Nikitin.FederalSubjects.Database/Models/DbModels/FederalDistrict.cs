e43886deb85f5f7775bbfc187a1ae3a5b0b9493d
Victor Maltsev <default8700k@gmail.com>
2022-05-21 19:55:58 +0300

    Add database project files.

---
+++ src/Nikitin.FederalSubjects.Database/Models/DbModels/FederalDistrict.cs
@@ -0,0 +1,15 @@
+﻿namespace Nikitin.FederalSubjects.Database.Models.DbModels
+{
+    public partial class FederalDistrict
+    {
+        public FederalDistrict()
+        {
+            FederalSubjects = new HashSet<FederalSubject>();
+        }
+
+        public short Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<FederalSubject> FederalSubjects { get; set; }
+    }
+}
---
+++ src/Nikitin.FederalSubjects.Database/Models/DbModels/FederalSubjectType.cs
@@ -0,0 +1,15 @@
+﻿namespace Nikitin.FederalSubjects.Database.Models.DbModels
+{
+    public partial class FederalSubjectType
+    {
+        public FederalSubjectType()
+        {
+            FederalSubjects = new HashSet<FederalSubject>();
+        }
+
+        public short Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<FederalSubject> FederalSubjects { get; set; }
+    }
+}
