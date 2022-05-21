5dbec6eea1f799e89c00bb8601ed96c3935eb862
Victor Maltsev <default8700k@gmail.com>
2022-05-21 13:14:35 +0300

    Add VersionController.

---
+++ src/Nikitin.FederalSubjects.WebService/Controllers/VersionController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nikitin.FederalSubjects.WebService.Models;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Reflection;
+
+namespace Nikitin.FederalSubjects.WebService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Produces("application/json")]
+public class VersionController : ControllerBase
+{
+    [HttpGet]
+    [SwaggerResponse(StatusCodes.Status200OK, Type = typeof(VersionModel))]
+    public IActionResult GetVersion() =>
+        new OkObjectResult(
+            new VersionModel
+            {
+                Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString()
+            }
+        );
+}
---
+++ src/Nikitin.FederalSubjects.WebService/Models/VersionModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Nikitin.FederalSubjects.WebService.Models;
+
+public record class VersionModel
+{
+    public string? Version { get; init; }
+}
