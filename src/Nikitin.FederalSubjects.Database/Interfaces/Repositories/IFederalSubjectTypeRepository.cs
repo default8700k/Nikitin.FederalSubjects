d6a1b036016ff066e6d4fdb278a751f23d9231bb
Victor Maltsev <default8700k@gmail.com>
2022-06-11 20:44:40 +0300

    Add repositories.

---
+++ src/Nikitin.FederalSubjects.Database/Interfaces/Repositories/IFederalSubjectTypeRepository.cs
@@ -0,0 +1,8 @@
+﻿using Nikitin.FederalSubjects.Database.Models;
+
+namespace Nikitin.FederalSubjects.Database.Interfaces.Repositories;
+
+public interface IFederalSubjectTypeRepository
+{
+    public Task<IEnumerable<FederalSubjectTypeModel>> GetFederalSubjectTypesAsync();
+}
---
+++ src/Nikitin.FederalSubjects.Database/Repositories/FederalDistrictRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nikitin.FederalSubjects.Database.Interfaces.Repositories;
+using Nikitin.FederalSubjects.Database.Models;
+
+namespace Nikitin.FederalSubjects.Database.Repositories;
+
+public class FederalDistrictRepository : IFederalDistrictRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public FederalDistrictRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<FederalDistrictModel>> GetFederalDistrictsAsync() =>
+        await _dbContext.FederalDistricts
+            .Select(x =>
+                new FederalDistrictModel
+                {
+                    Id = x.Id,
+                    Name = x.Name
+                }
+            )
+            .ToListAsync();
+}
