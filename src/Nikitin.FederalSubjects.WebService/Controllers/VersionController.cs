590ca33c112e41825c12894a0b06e765986e6c1d
default8700k <default8700k@gmail.com>
2022-08-14 13:39:39 +0300

    add version response

---
+++ src/Nikitin.FederalSubjects.WebService/Controllers/VersionController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+using System.Reflection;
+
+namespace Nikitin.FederalSubjects.WebService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class VersionController : ControllerBase
+{
+    [HttpGet]
+    [SwaggerResponse(StatusCodes.Status200OK, Description = "Success", Type = typeof(VersionDto))]
+    public IActionResult GetVersion()
+    {
+        var versionDto = CreateVersionDto();
+        return new JsonResult(versionDto);
+
+        static VersionDto CreateVersionDto() =>
+            new()
+            {
+                Version = Assembly.GetExecutingAssembly().GetName().Version?.ToString()
+            };
+    }
+
+    public record class VersionDto
+    {
+        public string? Version { get; init; }
+    }
+}
